955f8fe1d62b3c37e51c336bf7cd53454d188a0f
Martin Z <mzenze@gmail.com>
2023-06-30 15:39:53 -0300

    working!

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,29 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+  modules: [
+    '@nuxtjs/i18n',
+  ],
+  i18n: {
+    strategy: 'no_prefix',
+    defaultLocale: 'en',
+    langDir: 'locales',
+    // lazy: true,
+    locales: [
+      {
+        code: 'en',
+        iso: 'en',
+        name: 'English',
+        file: 'en.json'
+        // file: 'en.yml,'
+      },
+      {
+        code: 'es',
+        iso: 'es',
+        name: 'Español',
+        file: 'es.json'
+        // file: 'es.yml'
+      },
+    ]
+  }
 })
---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>Home</h1>
+  <p>{{ t('welcome') }}</p>
+  <h2>{{ t('flag') }}</h2>
+  <button @click="onLanguageChange">{{ locale }}</button>
+</template>
+
+<script setup>
+import { useI18n } from '#imports'
+
+const { locale, t, availableLocales } = useI18n()
+
+const onLanguageChange = () => {
+  cycleLocale()
+}
+
+const cycleLocale = () => {
+  const locales = availableLocales
+  locale.value = locales[(locales.indexOf(locale.value) + 1) % locales.length]
+}
+</script>
